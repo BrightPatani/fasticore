b70c5afdee15634b0a31fea4f8eb37fbd225caca
BrightPatani <brightpatani98@gmail.com>
2024-08-04 20:21:49 +0100

    Fasticore

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('side-menu').style.left = '0';
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('side-menu').style.left = '-250px';
+});
+
+
+document.getElementById('scroll-left').addEventListener('click', function() {
+    document.getElementById('services-content').scrollBy({
+        left: -300,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('scroll-right').addEventListener('click', function() {
+    document.getElementById('services-content').scrollBy({
+        left: 300,
+        behavior: 'smooth'
+    });
+});
