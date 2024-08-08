851b23a45d479defe9de06311bb49cf00bd8dec4
BrightPatani <brightpatani98@gmail.com>
2024-08-08 22:57:30 +0100

    Fasticore

--- js/script.js
+++ js/script.js
@@ -20,3 +20,34 @@ document.getElementById('scroll-right').addEventListener('click', function() {
         behavior: 'smooth'
     });
 });
+
+
+
+// scroll to top 
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("ScrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("ScrollToTopBtn").style.display = "none";
+    }
+}
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+// Smooth scroll to the top when the user clicks the button
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
